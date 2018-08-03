d7e43e2189bec00ffa7d5c2d4df66f27638d0ae4
David Ding <dding@pro-vision.de>
2018-08-03 11:36:04 +0200

    add git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,8 +12,13 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias gcm='git commit -m'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias ga='git add'
+alias ga.='git add .'
+alias gdel="git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d"
+alias gDel="git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D"
