b4399bbc97934ea51c4ef0ac71c5fc6038ffc289
David Ding <dding@hubspot.com>
2021-11-04 09:45:49 +0000

    Follow symlinks with cd; add git diff against upstream alias;

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,6 +12,7 @@ alias gm='git merge'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gdup='git diff @{upstream}'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gca='git commit -a'
